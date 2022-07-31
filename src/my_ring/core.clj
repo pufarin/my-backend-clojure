b02c263f5f06b4339354f8fd7151edce5f006485
pufarin <mail@gabrielkovacs.net>
2022-07-31 11:53:17 +0200

    added server reload

--- src/my_ring/core.clj
+++ src/my_ring/core.clj
@@ -1,10 +1,15 @@
 (ns my-ring.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :as reload]))
 
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body "Hello Clojure, Hellllllo Ring!"})
+   :body "Hello Clojure, Helwwwwwlll3333333llo Ring!"})
 
 (defn -main []
-  (jetty/run-jetty handler {:port 3000}))
+  (jetty/run-jetty (reload/wrap-reload #'handler) {:port 3000
+                                                   :join? false}))
+
+(comment
+  (-main))
